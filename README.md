23d785611cd91b06813a527142c3f3085e07fbfd
Rafael Rodrigues <rafaelpascoalrodrigues@gmail.com>
2019-03-14 10:03:34 -0300

    Add viewer method to show information based on navegation

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # realm
 
-### Core process
+## Core process
 
 Requests will be redirect to `index.php` by Apache2 Rewrite Module and the path tree will be converted in query string splitted as:
 
@@ -13,3 +13,41 @@ Include Files, as images and scripts, that will be ignored in that rule:
 - scripts: `.js`
 - stylesheets: `css`, `less`
 - images: `png`
+
+
+## Domains
+
+The _Domains_ is the area to develop the enviroment. The folder structure can have 3 levels: domain, subdomain and request. Domain and Subdomain levels can be a folder used to contain the lower levels.
+
+When a domain, subdomain, or request is called, a file `.php` and a file .`html` will be searched to be used. The file `.php`, if it exists, will be executed before and the file .`html` after, if it exists.
+
+If the call is invalid, a ***default*** file will be search to be used instead show a *Not Found* return.
+
+The Domain may have a ***class*** folder where classes can be created and autoloaded on demand. The class `nameserver` must be the Domain name.
+
+Structure Sample:
+
+- ***domains/***
+    - domain01/
+        - ***classes***/
+            - class01.php
+            - class02.php
+        - default.html
+        - default.php
+        - subdomain01/
+            - default.php
+            - request01.html
+            - request02.html
+            - request02.php
+            - request03.html
+    - domain02/
+        - subdomain02.php
+        - subdomain02.php
+    - domain03.php
+
+
+### Add a git submodule as Domains
+
+Use this command to add a git repository as submodule to Domains:
+
+    git submodule add <repository path> domains
--- realm/viewer.php
+++ realm/viewer.php
@@ -15,7 +15,79 @@ class Viewer {
 
 
     public function Show() {
-        self::DefaultNotFound();
+        $this->realm->router = self::FindDomainFiles($this->realm->domain, $this->realm->subdomain, $this->realm->request);
+
+        if ($this->realm->router->code->exists) {
+            include $this->realm->router->code->path;
+        }
+
+        if ($this->realm->router->template->exists) {
+            include $this->realm->router->template->path;
+        }
+
+        if (!$this->realm->router->code->exists && !$this->realm->router->template->exists) {
+            self::DefaultNotFound();
+        }
+
+        return;
+    }
+
+    private static function FindDomainFiles($domain, $subdomain, $request) {
+        $files = array();
+
+        $path = DOMAINS;
+        if ($domain != "") {
+            $path .= DIRECTORY_SEPARATOR . $domain;
+            if ($subdomain != "") {
+                $path .= DIRECTORY_SEPARATOR . $subdomain;
+                if ($request != "") {
+                    $path .= DIRECTORY_SEPARATOR . $request;
+                }
+            }
+        }
+
+
+        $code_exists = file_exists($path . '.php');
+        $template_exists = file_exists($path . '.html');
+        if ($code_exists || $template_exists) {
+            $files['code'] = array(
+                'path' => $path . '.php',
+                'exists' => file_exists($path . '.php'),
+            );
+
+            $files['template'] = array(
+                'path' => $path . '.html',
+                'exists' => file_exists($path . '.html'),
+            );
+
+            return $files;
+        } else {
+            if ($request != "") {
+                $path = substr($path, 0, strrpos($path, DIRECTORY_SEPARATOR));
+            }
+
+            for ($lifespan = 10; $lifespan > 0; $lifespan--) {
+                $code_exists = file_exists($path . DIRECTORY_SEPARATOR . 'default.php');
+                $template_exists = file_exists($path . DIRECTORY_SEPARATOR . 'default.html');
+                if ($code_exists || $template_exists) {
+                    $files['code'] = array(
+                        'path' => $path . DIRECTORY_SEPARATOR . 'default.php',
+                        'exists' => file_exists($path . DIRECTORY_SEPARATOR . 'default.php'),
+                    );
+
+                    $files['template'] = array(
+                        'path' => $path . DIRECTORY_SEPARATOR . 'default.html',
+                        'exists' => file_exists($path . DIRECTORY_SEPARATOR . 'default.html'),
+                    );
+
+                    return $files;
+                }
+
+                $path = substr($path, 0, strrpos($path, DIRECTORY_SEPARATOR));
+            }
+        }
+
+        return $files;
     }
 
 
